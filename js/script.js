fb23a37ce9b2f3c20f83a36583cf288e80afeed9
fishhead <egor@nefedov.org>
2021-10-16 17:51:22 +0300

    Первая практика

--- js/script.js
+++ js/script.js
@@ -1,56 +1,27 @@
 "use strict";
 
-// alert('Hey!');
 
-let userNumbers = {
-    number1: +prompt('Введите первое число:', ''),
-    number2: +prompt('Введите вторе число:', ''),
-    operation: prompt('Введите операцию:', '')
+let initQuestions = {
+    userName: prompt('Как вас зовут?', ''),
+    watchhedFilms: +prompt('Сколько фильмов вы уже посмотрели?', ''),
+    watchedMovieLast: prompt('Какой фильм посмотрели последним?', ''),
+    watchedMovieLastStars: +prompt('Какую  оценку дадите?', ''),
+    watchedMovieLast2: prompt('Какой фильм посмотрели за ним?', ''),
+    watchedMovieLastStars2: +prompt('Какую  оценку дадите этому фильму?', ''),
+    isPrivate: confirm('Сделать ответы публичными или нет?')
 };
-console.log(`Input variables: ${userNumbers.number1}, ${userNumbers.number2}, ${userNumbers.operation}`);
-console.log(`Types of variables: ${typeof(userNumbers.number1)}, ${typeof(userNumbers.number2)}, ${typeof(userNumbers.operation)}`);
 
-const operators = ["*", "/", "+", "-"];
-let resultOfOperation;
+let personalMovieDB = {};
+personalMovieDB[initQuestions.userName] = {
+    count: initQuestions.watchhedFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    private: initQuestions.isPrivate
+};
 
-if (isNaN(userNumbers.number1) == false && isNaN(userNumbers.number2) == false) {
-    for (var userInput in operators) {
-        if (operators.includes(userNumbers.operation)) {
-            console.log('We are found operator in list -- ' + userNumbers.operation);
-            if (userNumbers.operation === "*") {
-                resultOfOperation = userNumbers.number1 * userNumbers.number2;
-                console.log(`Result*: ${resultOfOperation}`);
-                document.write(`Результат: ${resultOfOperation}`);
-                break;
-            } else if (userNumbers.operation === "/") {
-                resultOfOperation = userNumbers.number1 / userNumbers.number2;
-                console.log(`Result/: ${resultOfOperation}`);
-                document.write(`Результат: ${resultOfOperation}`);
-                break;
-            } else if (userNumbers.operation === "+") {
-                resultOfOperation = userNumbers.number1 + userNumbers.number2;
-                console.log(`Result+: ${resultOfOperation}`);
-                document.write(`Результат: ${resultOfOperation}`);
-                break;
-            } else if (userNumbers.operation === "-") {
-                resultOfOperation = userNumbers.number1 - userNumbers.number2;
-                console.log(`Result-: ${resultOfOperation}`);
-                document.write(`Результат: ${resultOfOperation}`);
-                break;
-            } else {
-                resultOfOperation = 'Operator is ok, but result is unknown';
-            }
-        } else {
-            resultOfOperation = 'Unknown operator';
-            console.log(`Checking operator ${operators[userInput]}, we are looking for ${userNumbers.operation}`);
-        }
-    }
-} else {
-    resultOfOperation = 'Both or one of operators is not numbers';
-}
+personalMovieDB[initQuestions.userName].movies[initQuestions.watchedMovieLast] = initQuestions.watchedMovieLastStars;
+personalMovieDB[initQuestions.userName].movies[initQuestions.watchedMovieLast2] = initQuestions.watchedMovieLastStars2;
 
-if (typeof (resultOfOperation) != 'number') {
-    alert(`You have an error: ${resultOfOperation}`);
-} else {
-    alert(`Your result: ${resultOfOperation}`);
-}
+console.log(`All users DB:  ${JSON.stringify(personalMovieDB)}`);
+console.log(`Current user answers:  ${JSON.stringify(personalMovieDB[initQuestions.userName])}`);
